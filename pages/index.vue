32a0011d2a156f85c47f9eb0b94c92419f44d983
RadiMaulanaTsaqif <113565405+RadiMT33@users.noreply.github.com>
2023-11-15 15:21:10 +0700

    index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,25 @@
 <template>
-  <br>
-  <h2>Riwayat Kunjungan</h2>
-  <NuxtLink to="/tambah" class="btn btn-outline-secondary">Isi buku Tamu</NuxtLink>
+  <h2><center>Riwayat kunjungan</center></h2>
+  <NuxtLink to="/tambah">Isi Kunjungan</NuxtLink>
   <div>
-    <br>
-    <table class="table table-striped">
-      <thead class="table-dark">
+    <table class="table">
+      <thead class="table-primary">
         <tr>
           <th>#</th>
-          <th>TANGGAL DAN WAKTU</th>
-          <th>NAMA</th>
-          <th>ANGGOTA</th>
-          <th>KEPERLUAN</th>
+          <th>tgl</th>
+          <th>nama</th>
+          <th>anggota</th>
+          <th>keperluan</th>
+          <th>kelas</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="pengunjung in visitors" :key="pengunjung.id">
-          <td>{{ Pengunjung.id }}</td>
-          <td>{{ pengunjung.tanggal }}</td>
+          <td>{{ pengunjung.id }}</td>
           <td>{{ pengunjung.nama }}</td>
           <td>{{ pengunjung.anggota }}</td>
           <td>{{ pengunjung.keperluan }}</td>
+          <td>{{ pengunjung.kelas }}</td>
         </tr>
       </tbody>
     </table>
@@ -28,12 +27,14 @@
 </template>
 
 <script setup>
-const client =useSupabaseClient();
-const visitors = ref([]);
+const client=useSupabaseClient()
+const visitors=ref([])
 
-async function getdata() {
-  const { data, error } = await client.from("pengunjung").select("*");
-  if (data) visitors.value = data;
+async function getData(){
+  const{data,error}= await client
+  .from("pengunjung")
+  .select("*")
+  if (data)visitors.value = data
 }
-onMounted(()=> getdata());
+onMounted(() => getData())
 </script>
