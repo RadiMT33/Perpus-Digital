741452b20a26984832c257526a78ff5bc48001cf
RadiMaulanaTsaqif <113565405+RadiMT33@users.noreply.github.com>
2023-11-15 15:21:56 +0700

    tambah.vue

--- pages/tambah.vue
+++ pages/tambah.vue
@@ -1,41 +1,57 @@
 <template>
-    <div class="mb-3">
-        <br>
-        <h2>Isi buku Tamu</h2>
-        <br>
-        <form @submit.prevent="Simpankunjungan">
-            <input v-model="form.nama" type="text" placeholder="Nama ..." required/>
-            <br>
-            <select v-model="form.anggota">
-                <option value="">Pilih Anggota</option>
-                <option v-for="a in anggota" :key="a.id" :value="a.id">{{ a.nama }}</option>
-            </select>
-            <div v-if="form.angggota == 'siswa'" class="mb-3"></div>
-            <br>
-            <textarea v-model="form.keperluan" cols="30" rows="10" placeholder="Keperluan ..."></textarea>
-            <br>
-            <button type="submit" class="btn btn-primary">Kirim</button>
-            <NuxtLink to="/" class="btn btn-outline-secondary">Kembali</NuxtLink>
-        </form>
+<div class="row">
+    <div class="col">
+        <h2><center>isi pengunjung</center></h2>
+        <div v-if="sukses" class="alert alert-succes">berhasil simpan</div>
+        <form @submit.prevent="simpan()">
+        <div class="mb-3">
+            <select v-models="form.anggota" class="from-control">
+            <option value="">pilih keanggotaan</option>
+            <option value="SISWA">SISWA</option>
+            <option value="GURU">GURU</option>
+            <option value="STAFF">STAFF</option>
+        </select>
+        </div>
+        <div v-if="form.anggota == 'SISWA'" class="mb-3">
+            <input v-model="from.kelas" type="text" class="from-control" placeholder="kelas">
+        </div>
+        <div class="mb-3">
+            <texarea v-model="from.keperluan" cols="30" rows="10" placeholder="keperluan" class="from-control"></texarea>
+        </div>
+        <button type="submit" class="btn btn-outline-primary">kirim</button>
+        <NuxtLink to="/">kembali</NuxtLink>
+    </form></div>
     </div>
 </template>
 
 <script setup>
 const supabase = useSupabaseClient();
-const anggota = ref([]);
 const form = reactive({ 
     nama: "", 
     anggota: "", 
-    keperluan: "",
-});
+    kelas: "",
+    keperluan: ""
+})
+const sukses = ref(false)
 
-async function getAnggota(){
+async function simpan(){
+    sukses.value=false
+    const {error}= await supabase
+    .from('penggunjung')
+    .insert(from)
+    if (data){
+        sukses.value=true
+    }
+}
+
+async function getanggota(){
     const { data, error } = await supabase.from("anggota").select();
     if (data) anggota.value = data;
 }
 async function Simpankunjungan(){
-    // console.log(form);
-    const { error } = await supabase.from("pengunjung").insert(form);
+    const { error } = await supabase
+    .from('pengunjung')
+    .insert(form);
 }
-onMounted(() => getAnggota());
+onMounted(() => getanggota());
 </script>
