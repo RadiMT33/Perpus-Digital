4d5a6040a735f020503550c3ad49ad6a4c451576
RadiMaulanaTsaqif <113565405+RadiMT33@users.noreply.github.com>
2023-11-15 15:24:40 +0700

    default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="container">
-    <div class="row bg-primary text
-    <h1>Buku Pengunjung Perpustakaan</h1>
-    <hr>
+    <div class="row bg-primary text-light align-items-start">
+      <div class="col-lg-1">
+        <img src="~/assets/image/loho.png" alt="" width="80" height="80" class="d-inline-block align-text-top left-20"/>
+      </div>
+      <div class="col-bg bg-primary">
+        <h1 class>BUKU PENGUNJUNG PERPUSTAKAAN</h1>
+        <p>
+        JL. Depok, Sukamenak, kec. Purbaratu, kab.Tasikmalaya, Jawa Barat
+        </p>  
+      </div>
+      </div>
     <slot/>
   </div>
 </template>
