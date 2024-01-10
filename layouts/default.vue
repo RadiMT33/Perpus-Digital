b67b55588b50ab6838ea31bc4317bdc1500b4a02
RadiMaulanaTsaqif <113565405+RadiMT33@users.noreply.github.com>
2024-01-10 11:05:04 +0700

    default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,27 @@
 <template>
-  <div class="container">
-    <div class="row bg-primary text-light align-items-start">
-      <div class="col-lg-1">
+  <div class="container-fluud">
+    <div class="row bg-success text-light>
+      <div class="col-lg-2">
         <img src="~/assets/img/logoo.png"
         alt=""
-        width="80"
-        height="80"
-        class="d-inline-block align-text-top left-20">
+        width="150"
+        height="150"
+        class="p-3"/>
       </div>
       <div class="col-lg">
         <h1>BUKU PENGUNJUNG PERPUSTAKAAN</h1>
         <P>
-          JL.DEPOK,SUKAMENAK,KEC.PURBARATU,KAB.TASIKMALAY,JAWA BARAT
+          JL.DEPOK,SUKAMENAK,KEC.PURBARATU,KAB.TASIKMALAYA,JAWA BARAT
         </P>
       </div>
+    <div class="col-lg-2">
+     <img src="~/assets/img/logoo.png"
+        alt=""
+        width="150"
+        height="150"
+        class="d-inline-block align-text-top right= 20"/>
     </div>
+    <div>
     <slot />
 </div>
 </template>
